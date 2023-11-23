e82931e4bfc74dbb6b3041c64b96dad56ea219af
Kumaran <kumarans1508@gmaill.com>
2023-11-23 21:44:38 +0530

    Create bottom navigation

--- app/src/main/java/com/teknophase/chat/navigation/AppNavRoutes.kt
+++ app/src/main/java/com/teknophase/chat/navigation/AppNavRoutes.kt
@@ -1,7 +1,9 @@
 package com.teknophase.chat.navigation
 
-enum class AppNavRoutes {
-    CHAT,
-    CALLS,
-    PROFILE
+import com.teknophase.chat.R
+
+enum class AppNavRoutes(val title: String,val icon: Int, val route: String) {
+    CHAT("Chats", R.drawable.icon_chat, "chat"),
+    CALLS("Calls", R.drawable.icon_call, "call_history"),
+    PROFILE("Profile",R.drawable.icon_profile ,"profile")
 }
